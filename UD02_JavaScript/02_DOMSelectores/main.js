a0df2ba8dbd1d85a655778476c09eb64bf2dbf7a
Miguel CR <camposmiguel@gmail.com>
2018-10-04 10:29:28 +0200

    selector y evento click

---
+++ UD02_JavaScript/02_DOMSelectores/main.js
@@ -0,0 +1,20 @@
+
+window.onload = function() {
+    var el = document.querySelector(".descripcion");
+    console.log(el);
+
+    var inputNombre = document.querySelector("#nombre");
+    console.log(inputNombre);
+
+    // Asocio el inputNombre el evento click
+    // Para ello le asigno al método onclick
+    // una función anónima que recibe por parámetro
+    // las propiedades el evento
+    var btnEnvio = document.querySelector("#botonEnvio");
+    btnEnvio.onclick = function(event) { 
+        console.log(event);
+        alert('El usuario ha escrito:' + inputNombre.value);
+    }
+    
+    
+}
